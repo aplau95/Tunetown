5453cf580319aa37f23fd3177be204ad6f6c6954
Kevin Vincent <kevinsundar@gmail.com>
2018-09-28 21:39:37 -0700

    Cleaned up

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,25 +1,22 @@
 package application;
 
 import java.io.BufferedInputStream;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.Base64;
 import java.util.List;
 
+import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.sound.sampled.FloatControl;
 
 import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -34,65 +31,82 @@ import com.wrapper.spotify.SpotifyApi;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
-import javafx.util.Duration;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
-import javafx.scene.media.MediaView;
+import javazoom.jl.player.advanced.AdvancedPlayer;
 
-import javazoom.jl.player.Player;;
-
+class SpotifyAccessor {
+	
+	private String accessToken;
+	private CloseableHttpClient client;
+	
+	public SpotifyAccessor(String clientId, String clientSecret) throws ClientProtocolException, IOException {
+		
+		client = HttpClientBuilder.create().build();
+		
+		HttpPost httppost = new HttpPost("https://accounts.spotify.com/api/token");
+		
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("grant_type", "client_credentials"));
+		httppost.setEntity(new UrlEncodedFormEntity(params));
+		
+		String encoding = Base64.getEncoder().encodeToString((clientId+":"+clientSecret).getBytes());
+		httppost.setHeader("Authorization", "Basic " + encoding);
+		
+		CloseableHttpResponse response = client.execute(httppost);
+		String bodyAsString = EntityUtils.toString(response.getEntity());
+		
+		JSONObject obj = new JSONObject(bodyAsString);
+		
+		accessToken = obj.getString("access_token");
+		
+	}
+	
+	public JSONObject getSampleTrack() throws ClientProtocolException, IOException {
+		
+		HttpGet httpget = new HttpGet("https://api.spotify.com/v1/tracks/2TpxZ7JUBn3uw46aR7qd6V");
+		httpget.setHeader("Authorization","Bearer "+accessToken);
+		
+		CloseableHttpResponse response = client.execute(httpget);
+		String bodyAsString = EntityUtils.toString(response.getEntity());
+		
+		return new JSONObject(bodyAsString);
+	}
+	
+}
 
 public class Main extends Application {
 	
-	private static MediaPlayer mediaPlayer;
-	private static Media hit;
+	public static InputStream openStream(String myUrl) throws IOException {
+	    final URL url = new URL(myUrl);
+	    final URLConnection con = url.openConnection();
+	    con.setRequestProperty("User-Agent", "My Client");
+	    return new BufferedInputStream(con.getInputStream());
+	}
 	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			
-			CloseableHttpClient client = HttpClientBuilder.create().build();
+			BorderPane root = new BorderPane();
+ 			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
 			
-			HttpPost httppost = new HttpPost("https://accounts.spotify.com/api/token");
+			SpotifyAccessor spotify = new SpotifyAccessor("160b683f23e946ed8000ec438e36890a",
+					"efa3a5718c6a49acb3828305c3a01c7b");
 			
-			List<NameValuePair> params = new ArrayList<NameValuePair>();
-			params.add(new BasicNameValuePair("grant_type", "client_credentials"));
-			httppost.setEntity(new UrlEncodedFormEntity(params));
-			
-			String encoding = Base64.getEncoder().encodeToString(("160b683f23e946ed8000ec438e36890a:efa3a5718c6a49acb3828305c3a01c7b").getBytes());
-			httppost.setHeader("Authorization", "Basic " + encoding);
-			
-			CloseableHttpResponse response = client.execute(httppost);
-			String bodyAsString = EntityUtils.toString(response.getEntity());
-			
-			System.out.println(bodyAsString);
-			
-			JSONObject obj = new JSONObject(bodyAsString);
-			String token = obj.getString("access_token");
-			
-			System.out.println(token);
-			
-			HttpGet httpget = new HttpGet("https://api.spotify.com/v1/tracks/2TpxZ7JUBn3uw46aR7qd6V");
-			httpget.setHeader("Authorization","Bearer "+token);
-			
-			response = client.execute(httpget);
-			bodyAsString = EntityUtils.toString(response.getEntity());
-			obj = new JSONObject(bodyAsString);
-			String preview_url = obj.getString("preview_url");
-			System.out.println(preview_url);
+			String previewUrl = spotify.getSampleTrack().getString("preview_url");
 			
 			try {
-				URL url = new URL(preview_url);	
-				Player myPlayer = new Player(url.openStream());
+				URL url = new URL(previewUrl);	
+				AdvancedPlayer myPlayer = new AdvancedPlayer(url.openStream());
 				myPlayer.play();
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-					
-
+		
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
