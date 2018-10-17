9722305068300ae30585e0b210053731f8fb3d04
Kevin Vincent <kevinsundar@gmail.com>
2018-10-17 15:36:27 -0700

    Closes #3 Moved AudioPlayer and SpotifyAccessor to other files, added prototype for playing audio

---
+++ src/application/LoopingAudioPlayer.java
@@ -0,0 +1,142 @@
+package application;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.concurrent.Callable;
+import java.util.function.Function;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.FloatControl;
+import javax.sound.sampled.SourceDataLine;
+
+class LoopingAudioPlayer implements Runnable {
+	
+	private URL streamUrl;
+	private boolean stop = false;
+	private Runnable onStop;
+	
+	public LoopingAudioPlayer(URL url) {
+		streamUrl = url;
+	}
+	
+	@Override
+	public void run() {
+		
+		AudioInputStream din = null;
+		SourceDataLine line = null;
+		
+	    try {
+	        AudioInputStream in = AudioSystem.getAudioInputStream(streamUrl);
+	        AudioFormat baseFormat = in.getFormat();
+	        AudioFormat decodedFormat = new AudioFormat(
+	                AudioFormat.Encoding.PCM_SIGNED,
+	                baseFormat.getSampleRate(), 16, baseFormat.getChannels(),
+	                baseFormat.getChannels() * 2, baseFormat.getSampleRate(),
+	                false);
+	        din = AudioSystem.getAudioInputStream(decodedFormat, in);
+	        DataLine.Info info = new DataLine.Info(SourceDataLine.class, decodedFormat);
+	        line = (SourceDataLine) AudioSystem.getLine(info);
+
+	        if(line != null) {
+	            line.open(decodedFormat);
+	            byte[] data = new byte[4096];
+	            // Start
+	            line.start();
+	            
+	            FloatControl control = (FloatControl) line.getControl(FloatControl.Type.MASTER_GAIN);
+
+	            int totalBytes = 0;
+	            int nBytesRead;
+	            
+	            AudioFormat fmt = din.getFormat();
+	            long totalFrames = din.getFrameLength();
+	            long frameSize = fmt.getFrameSize();
+	            double totalSeconds = (double) totalFrames / fmt.getSampleRate();
+	            
+	            double initialGain = control.getValue();
+	            double startFadeOut = 29;
+	            boolean startedStop = false;
+	            
+	            while ((nBytesRead = din.read(data, 0, data.length)) != -1) {
+	            	
+	            	totalBytes += nBytesRead;
+		            long framesRead = totalBytes / frameSize;
+		            double elapsedSeconds = ((double) framesRead / (double) totalFrames) * totalSeconds;
+		            
+		            if(stop && !startedStop) {
+		            	if(elapsedSeconds < 29) {
+		            		startFadeOut = elapsedSeconds;
+		            	}
+		            	startedStop = true;
+		            } 
+		            
+		            double gain = getFadeGain(0,1,startFadeOut,startFadeOut+1,
+		            		control.getMinimum(),initialGain,
+		            		elapsedSeconds);
+		            
+		            if(startedStop && gain == control.getMinimum()) {
+		            	break;
+		            }
+		            
+		            control.setValue((float) gain);
+	                
+		            line.write(data, 0, nBytesRead);
+	            }
+	        }
+
+	    }
+	    catch(Exception e) {
+	        e.printStackTrace();
+	    }
+	    finally {
+	        if(din != null) {
+	            try { din.close(); } catch(IOException e) { }
+	        }
+	        if(line != null) {
+	            line.drain();
+	            line.stop();
+	            line.close();
+	        }
+	        
+	        if(!stop) {
+	        	run();
+	        } else if(onStop != null) {
+	        	onStop.run();
+	        }
+	    }
+	}
+	
+	public void stop() {
+		this.stop = true;
+	}
+	
+	public void stop(Runnable callback) {
+		stop();
+		this.onStop = callback;
+	}
+	
+	private double getFadeGain(double startA, double endA, double startB, double endB, double min, double max, double elapsedSeconds) {
+        double gainStart = rangeMap(elapsedSeconds, startA, endA, min, max);
+        double gainStop = rangeMap(elapsedSeconds, startB, endB, max, min);
+        
+        if(gainStop != max) {
+        	return gainStop;
+        }
+        
+        return gainStart;
+	}
+	
+	private double rangeMap(double oldValue, double oldMin, double oldMax, double newMin, double newMax) {
+		if(oldValue < oldMin) {
+			oldValue = oldMin;
+		} else if (oldValue > oldMax) {
+			oldValue = oldMax;
+		}
+		return ((oldValue - oldMin) / (oldMax - oldMin))
+		          * (newMax - newMin) + newMin;
+	}
+
+}
--- src/application/Main.java
+++ src/application/Main.java
@@ -1,33 +1,6 @@
 package application;
 
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.net.URL;
-import java.net.URLConnection;
-import java.util.ArrayList;
-import java.util.Base64;
-import java.util.List;
-
-import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
-import javax.sound.sampled.FloatControl;
-
-import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.util.EntityUtils;
-import org.json.JSONObject;
-
-import com.wrapper.spotify.SpotifyApi;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
@@ -39,84 +12,48 @@ import javafx.scene.control.TextField;
 import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 
-
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.TilePane;
-import javazoom.jl.player.advanced.AdvancedPlayer;
-
-class SpotifyAccessor {
-	
-	private String accessToken;
-	private CloseableHttpClient client;
-	
-	public SpotifyAccessor(String clientId, String clientSecret) throws ClientProtocolException, IOException {
-		
-		client = HttpClientBuilder.create().build();
-		
-		HttpPost httppost = new HttpPost("https://accounts.spotify.com/api/token");
-		
-		List<NameValuePair> params = new ArrayList<NameValuePair>();
-		params.add(new BasicNameValuePair("grant_type", "client_credentials"));
-		httppost.setEntity(new UrlEncodedFormEntity(params));
-		
-		String encoding = Base64.getEncoder().encodeToString((clientId+":"+clientSecret).getBytes());
-		httppost.setHeader("Authorization", "Basic " + encoding);
-		
-		CloseableHttpResponse response = client.execute(httppost);
-		String bodyAsString = EntityUtils.toString(response.getEntity());
-		
-		JSONObject obj = new JSONObject(bodyAsString);
-		
-		accessToken = obj.getString("access_token");
-		
-	}
-	
-	public JSONObject getSampleTrack() throws ClientProtocolException, IOException {
-		
-		HttpGet httpget = new HttpGet("https://api.spotify.com/v1/tracks/2TpxZ7JUBn3uw46aR7qd6V");
-		httpget.setHeader("Authorization","Bearer "+accessToken);
-		
-		CloseableHttpResponse response = client.execute(httpget);
-		String bodyAsString = EntityUtils.toString(response.getEntity());
-		
-		return new JSONObject(bodyAsString);
-	}
-	
-}
 
 public class Main extends Application {
 	
-	public static InputStream openStream(String myUrl) throws IOException {
-	    final URL url = new URL(myUrl);
-	    final URLConnection con = url.openConnection();
-	    con.setRequestProperty("User-Agent", "My Client");
-	    return new BufferedInputStream(con.getInputStream());
-	}
-	
 	@Override
 	public void start(Stage primaryStage) {
-		try {
-			
-			setupStage(primaryStage);
-			
-			SpotifyAccessor spotify = new SpotifyAccessor("160b683f23e946ed8000ec438e36890a",
-					"efa3a5718c6a49acb3828305c3a01c7b");
-			
-			String previewUrl = spotify.getSampleTrack().getString("preview_url");
+		
+		setupStage(primaryStage);
+		
+		new Thread(() -> {
 			
 			try {
-				URL url = new URL(previewUrl);	
-				AdvancedPlayer myPlayer = new AdvancedPlayer(url.openStream());
-				myPlayer.play();
+
+//Uncomment to play audio for 5 seconds
+//				SpotifyAccessor spotify = new SpotifyAccessor("160b683f23e946ed8000ec438e36890a",
+//						"efa3a5718c6a49acb3828305c3a01c7b");
+//				
+//				String previewUrl = spotify.getSampleTrack().getString("preview_url");
+//				
+//				LoopingAudioPlayer player = new LoopingAudioPlayer(new URL(previewUrl));
+//				
+//				//Start Playing
+//				Thread audioThread = new Thread(player);
+//				audioThread.start();
+//				
+//				//Wait 5 seconds
+//				Thread.sleep(5000);
+//				
+//				//Stop and after it stops print exited
+//				player.stop(() -> {
+//					System.out.println("Exited");
+//				});
+			
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-		
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+			
+		}).start();
+	
 	}
 	
 	public void setupStage (Stage primaryStage) {
