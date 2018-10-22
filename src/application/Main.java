d638cf1be110a34d6c14b421acf88df8984fd9ba
Chanelle <ckaith97@yahoo.com>
2018-10-22 11:13:26 -0700

    Ticket #4 Switch scenes

--- src/application/Main.java
+++ src/application/Main.java
@@ -31,6 +31,7 @@ import com.wrapper.spotify.SpotifyApi;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -44,7 +45,11 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.TilePane;
+import javafx.scene.layout.VBox;
 import javazoom.jl.player.advanced.AdvancedPlayer;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 
 class SpotifyAccessor {
 	
@@ -119,54 +124,79 @@ public class Main extends Application {
 		}
 	}
 	
+	BorderPane root = new BorderPane();
+    Node subScene = GetHomeScene();
+    public Node GetNavigationPanel() {
+    	
+    	// Buttons
+        Button btnHome = new Button("Home");
+        Button btnDiscover = new Button("Discover");
+        Button btnPlaylist = new Button("Playlists");
+        Button btnFavorites = new Button("Favorites");
+    	
+    	TilePane navPanel = new TilePane(Orientation.HORIZONTAL);//,hgap, vgap);
+    	 navPanel.setAlignment(Pos.CENTER);
+         navPanel.setPrefColumns(4);
+         navPanel.getChildren().add(btnHome);
+         navPanel.getChildren().add(btnDiscover);
+         navPanel.getChildren().add(btnPlaylist);
+         navPanel.getChildren().add(btnFavorites);
+
+         // Set the padding of the TilePane     
+         navPanel.setStyle("-fx-padding: 10;");
+         navPanel.setStyle("-fx-border-style: solid inside;");
+         navPanel.setStyle("-fx-border-width: 2;");
+         navPanel.setStyle("-fx-border-insets: 5;");
+         navPanel.setStyle("-fx-border-radius: 5;");
+         navPanel.setStyle("-fx-border-color: blue;");
+
+         // Setup default subscene at the start of application
+         root.setCenter(subScene);
+         
+         // Change subscene with navigation panel
+         btnHome.setOnAction(new EventHandler<ActionEvent>() {
+         	public void handle(ActionEvent event) {
+         		subScene = GetHomeScene();
+         		root.setCenter(subScene);
+         	}
+         });
+         btnDiscover.setOnAction(new EventHandler<ActionEvent>() {
+         	public void handle(ActionEvent event) {
+         		subScene = GetDiscoverScene();
+         		root.setCenter(subScene);
+         	}
+         });
+         btnPlaylist.setOnAction(new EventHandler<ActionEvent>() {
+         	public void handle(ActionEvent event) {
+         		subScene = GetPlaylistScene();
+         		root.setCenter(subScene);
+         	}
+         });
+     	btnFavorites.setOnAction(new EventHandler<ActionEvent>() {
+         	public void handle(ActionEvent event) {
+         		subScene = GetFavoritesScene();
+         		root.setCenter(subScene);
+         	}
+         });
+     	
+    	return navPanel;
+    }
+    
 	public void setupStage (Stage primaryStage) {
 		// Create the Label for the Header
         Label headerLbl = new Label("TuneTown");
-        // Create the Label for the Input
-        Label leftLbl = new Label("Songs");
-        Button navHomeBtn = new Button("HOME");
-        Button navPlaylistBtn = new Button("PLAYLIST");
-        
-    	// Navigation pane
-        // Create the horizontal TilePane with a 10px spacing  
-        float hgap = 10f;
-        float vgap = 10f;
-        TilePane navPane = new TilePane(Orientation.HORIZONTAL);//,hgap, vgap);
-        navPane.setAlignment(Pos.CENTER);
-        navPane.setPrefColumns(4);
-        navPane.getChildren().add(new Button("Home"));
-        navPane.getChildren().add(new Button("Discover"));
-        navPane.getChildren().add(new Button("Playlist"));
-        navPane.getChildren().add(new Button("Favorites"));
 
-        // Set the padding of the TilePane     
-        navPane.setStyle("-fx-padding: 10;");
-        // Set the border-style of the TilePane
-        navPane.setStyle("-fx-border-style: solid inside;");
-        // Set the border-width of the TilePane
-        navPane.setStyle("-fx-border-width: 2;");
-        // Set the border-insets of the TilePane
-        navPane.setStyle("-fx-border-insets: 5;");
-        // Set the border-radius of the TilePane
-        navPane.setStyle("-fx-border-radius: 5;");
-        // Set the border-color of the TilePane
-        navPane.setStyle("-fx-border-color: blue;");
-		
-		BorderPane root = new BorderPane();
+    	// Navigation pane
+       
+        Node navPanel = GetNavigationPanel();
+        
 		root.setTop(headerLbl);
-		root.setLeft(leftLbl);
-		root.setBottom(navPane);
+		root.setBottom(navPanel);
 		
 		// Set alignment
 		BorderPane.setAlignment(headerLbl, Pos.TOP_CENTER);
-		BorderPane.setAlignment(leftLbl, Pos.TOP_CENTER);
-		BorderPane.setAlignment(navHomeBtn, Pos.BOTTOM_RIGHT);
-		BorderPane.setAlignment(navPane, Pos.BOTTOM_CENTER);
-		
- 
+		BorderPane.setAlignment(navPanel, Pos.BOTTOM_CENTER);
 
-		
-		
 		Scene scene = new Scene(root,400,800);
 		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 		
@@ -180,6 +210,48 @@ public class Main extends Application {
 		
 		//return;
 	}
+	
+	public Node GetHomeScene() {
+		Label lbl = new Label("HOME");
+
+		VBox root = new VBox();
+
+		root.getChildren().add(lbl);
+		root.setAlignment(Pos.CENTER);
+		
+		return root;
+	}
+	public Node GetPlaylistScene() {
+		Label lbl = new Label("PLAYLIST");
+
+		VBox root = new VBox();
+
+		root.getChildren().add(lbl);
+		root.setAlignment(Pos.CENTER);
+		
+		return root;
+	}
+	public Node GetDiscoverScene() {
+		Label lbl = new Label("DISCOVER");
+
+		VBox root = new VBox();
+
+		root.getChildren().add(lbl);
+		root.setAlignment(Pos.CENTER);
+		
+		return root;
+	}
+	public Node GetFavoritesScene() {
+		Label lbl = new Label("FAVORITES");
+
+		VBox root = new VBox();
+
+		root.getChildren().add(lbl);
+		root.setAlignment(Pos.CENTER);
+		
+		return root;
+	}
+	
 	
 	public static void main(String[] args) {
 		launch(args);
