645ffbff263e3ea25518ffa97ffb80218148ea8c
Kevin Vincent <kevinsundar@gmail.com>
2018-11-26 13:56:14 -0800

    Added Junit to pom.xml, Fixed tests, moved out of src

--- tst/application/controller/ControllerFactoryTests.java
+++ tst/application/controller/ControllerFactoryTests.java
@@ -1,39 +1,38 @@
-package tests;
+package application.controller;
 
-import org.junit.jupiter.api.Test;
+import application.FavoritesData;
+import org.junit.Test;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.junit.Assert.assertThat;
 
-import application.controller.ControllerFactory;
 import application.controller.Discover.DiscoverController;
 import application.controller.Favorites.FavoritesController;
 import application.controller.Home.HomeController;
 import application.controller.Playlists.PlaylistsController;
-import application.controller.Controller;
 
 class ControllerFactoryTests {
 
 	@Test
 	void testBuildHomeController() {
-		Controller c = ControllerFactory.build(ControllerFactory.Type.HOME);
+		Controller c = ControllerFactory.build(ControllerFactory.Type.HOME, new FavoritesData());
 		assertThat(c, instanceOf(HomeController.class));
 	}
 	
 	@Test
 	void testBuildDiscoverController() {
-		Controller c = ControllerFactory.build(ControllerFactory.Type.DISCOVER);
+		Controller c = ControllerFactory.build(ControllerFactory.Type.DISCOVER, new FavoritesData());
 		assertThat(c, instanceOf(DiscoverController.class));
 	}
 	
 	@Test
 	void testBuildFavoritesController() {
-		Controller c = ControllerFactory.build(ControllerFactory.Type.FAVORITES);
+		Controller c = ControllerFactory.build(ControllerFactory.Type.FAVORITES, new FavoritesData());
 		assertThat(c, instanceOf(FavoritesController.class));
 	}
 	
 	@Test
 	void testBuildPlaylistsController() {
-		Controller c = ControllerFactory.build(ControllerFactory.Type.PLAYLISTS);
+		Controller c = ControllerFactory.build(ControllerFactory.Type.PLAYLISTS, new FavoritesData());
 		assertThat(c, instanceOf(PlaylistsController.class));
 	}
 
